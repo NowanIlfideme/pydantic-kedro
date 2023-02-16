f254ff21127ab9d6a35d7b78801e82f29c3678d8
NowanIlfideme <git@nowan.dev>
2023-02-16 16:39:02 +0100

    Renamed model to avoid intersection.

--- src/test/test_ds_simple.py
+++ src/test/test_ds_simple.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 from pydantic_kedro import PydanticJsonDataSet
 
 
-class MyModel(BaseModel):
+class SimpleTestModel(BaseModel):
     """My very own model.
 
     NOTE: Since this is defined in `__main__`, this can only be loaded if ran in this notebook.
@@ -18,13 +18,15 @@ class MyModel(BaseModel):
     alter_ego: Optional[str] = None
 
 
-@pytest.mark.parametrize("mdl", [MyModel(name="user"), MyModel(name="Dr. Jekyll", alter_ego="Mr. Hyde")])
-def test_simple_model_json_rt(mdl: MyModel, tmpdir):
+@pytest.mark.parametrize(
+    "mdl", [SimpleTestModel(name="user"), SimpleTestModel(name="Dr. Jekyll", alter_ego="Mr. Hyde")]
+)
+def test_simple_model_json_rt(mdl: SimpleTestModel, tmpdir):
     """Tests whether a simple model survives a roundtripping."""
     paths = [f"{tmpdir}/model.json", "memory://in-mem-file.json"]
     for path in paths:
         ds = PydanticJsonDataSet(path)
         ds.save(mdl)
         m2 = ds.load()
-        assert isinstance(m2, MyModel)
+        assert isinstance(m2, SimpleTestModel)
         assert m2 == mdl
