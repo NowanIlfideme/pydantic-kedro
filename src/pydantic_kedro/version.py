61495d130b9b70ae826751dfb56e31fefbcbebc9
NowanIlfideme <git@nowan.dev>
2023-02-21 12:51:22 +0100

    Fix version getter.

--- src/pydantic_kedro/version.py
+++ src/pydantic_kedro/version.py
@@ -1,1 +1,15 @@
-__version__ = "0.0.1"
+"""Gets the version, either installed or dynamically."""
+
+__all__ = ["__version__"]
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("idea")
+except PackageNotFoundError:
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="../..", relative_to=__file__)
+    except Exception:
+        __version__ = "0.0.0"
