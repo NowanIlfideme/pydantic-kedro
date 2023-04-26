dc036b06d1683c354e52f8e2fce9a15bf0f74de1
NowanIlfideme <git@nowan.dev>
2023-04-26 19:04:07 +0200

    Added extras check for pydantic-yaml extra.

--- src/pydantic_kedro/__init__.py
+++ src/pydantic_kedro/__init__.py
@@ -11,5 +11,11 @@ __all__ = [
 from .datasets.folder import PydanticFolderDataSet
 from .datasets.json import PydanticJsonDataSet
 from .datasets.zip import PydanticZipDataSet
+from .extras import INSTALLED_YAML
 from .models import ArbModel
 from .version import __version__
+
+if INSTALLED_YAML:
+    from .datasets.yaml import PydanticYamlDataSet  # noqa
+
+    __all__.append("PydanticYamlDataSet")
