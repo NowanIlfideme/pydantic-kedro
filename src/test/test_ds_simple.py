cb126faac9bddde2830386b84589e117211d418f
NowanIlfideme <git@nowan.dev>
2023-04-26 19:24:30 +0200

    Updated test for simple model.

--- src/test/test_ds_simple.py
+++ src/test/test_ds_simple.py
@@ -1,8 +1,9 @@
-"""Tests the JSON dataset on a simple model."""
+"""Tests the datasets on a simple model."""
 
-from typing import Optional, Union
+from typing import Optional
 
 import pytest
+from kedro.io.core import AbstractDataSet
 from pydantic import BaseModel
 
 from pydantic_kedro import (
@@ -10,31 +11,39 @@ from pydantic_kedro import (
     PydanticJsonDataSet,
     PydanticZipDataSet,
 )
+from pydantic_kedro.extras import INSTALLED_YAML
 
 
 class SimpleTestModel(BaseModel):
     """My very own model.
 
-    NOTE: Since this is defined in `__main__`, this can only be loaded if ran in this notebook.
+    NOTE: Since this is defined in `__main__`, this can only be loaded if ran in this file.
     """
 
     name: str
     alter_ego: Optional[str] = None
 
 
-Kls = Union[PydanticFolderDataSet, PydanticJsonDataSet, PydanticZipDataSet]
+types = [PydanticJsonDataSet, PydanticFolderDataSet, PydanticZipDataSet]
+if INSTALLED_YAML:
+    from pydantic_kedro.datasets.yaml import PydanticYamlDataSet
+
+    types.append(PydanticYamlDataSet)
 
 
 @pytest.mark.parametrize(
     "mdl", [SimpleTestModel(name="user"), SimpleTestModel(name="Dr. Jekyll", alter_ego="Mr. Hyde")]
 )
-@pytest.mark.parametrize("kls", [PydanticJsonDataSet, PydanticFolderDataSet, PydanticZipDataSet])
-def test_simple_model_rt(mdl: SimpleTestModel, kls: Kls, tmpdir):
+@pytest.mark.parametrize("kls", types)
+def test_simple_model_rt(mdl: SimpleTestModel, kls: AbstractDataSet, tmpdir):  # type: ignore
     """Tests whether a simple model survives roundtripping."""
     paths = [f"{tmpdir}/model_on_disk", f"memory://{tmpdir}/model_in_memory"]
     for path in paths:
-        ds: Kls = kls(path)  # type: ignore
+        ds: AbstractDataSet = kls(path)  # type: ignore
         ds.save(mdl)
         m2 = ds.load()
         assert isinstance(m2, SimpleTestModel)
         assert m2 == mdl
+
+
+# @pytest.mark.skipif(INSTALLED_YAML, reason="Extra 'yaml' (`pydantic-yaml` library) is not installed.")
