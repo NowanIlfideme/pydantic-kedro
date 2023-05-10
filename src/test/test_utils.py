ed59369971de172ec33c772a2d5b470e00153bcc
NowanIlfideme <git@nowan.dev>
2023-05-10 17:00:17 +0200

    Added tests for the utils and standalone docs.

---
+++ src/test/test_utils.py
@@ -0,0 +1,21 @@
+"""Test utility functions, loading and saving models."""
+
+from pydantic import BaseModel
+
+from pydantic_kedro import load_model, save_model
+
+
+class MyModel(BaseModel):
+    """My model."""
+
+    x: str
+
+
+def test_utils_load_save(tmpdir: str):
+    """Minimal test for load/save."""
+    # using memory to avoid tempfile
+    save_model(MyModel(x="example"), f"{tmpdir}/model")
+
+    obj = load_model(f"{tmpdir}/model")
+    assert isinstance(obj, MyModel)
+    assert obj.x == "example"
