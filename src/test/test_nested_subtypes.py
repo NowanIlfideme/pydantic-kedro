90bbd8df0a6b48e308cf833d0d901903d2639775
NowanIlfideme <git@nowan.dev>
2023-06-01 16:07:12 +0200

    Created minimal failing test case.

---
+++ src/test/test_nested_subtypes.py
@@ -0,0 +1,64 @@
+"""Tests for values which are subclasses of the Pydantic field types."""
+
+from abc import abstractmethod
+from typing import Literal
+
+import pytest
+from pydantic import BaseModel
+
+from pydantic_kedro import load_model, save_model
+
+
+class AbstractBaz(BaseModel):
+    """Abstract base model which is used as an interface.
+
+    This can occur with ANY model type, however with abstract models it's much more apparent.
+    You can't instantiate models with
+    """
+
+    foo: str
+
+    @abstractmethod
+    def get_baz(self) -> str:
+        """Return baz value."""
+
+
+class AlwaysBaz(AbstractBaz):
+    """Model that returns baz as "baz"."""
+
+    def get_baz(self) -> str:
+        """Return baz as "baz"."""
+        return "baz"
+
+
+class CopyBaz(AbstractBaz):
+    """Model that copies foo into baz."""
+
+    def get_baz(self) -> str:
+        """Return baz as a copy of foo."""
+        return self.foo
+
+
+class Bazzifier(BaseModel):
+    """Higher-level model with nested 'bazzifier' value."""
+
+    maker: AbstractBaz
+
+
+@pytest.mark.parametrize("format", ["auto", "zip", "folder", "yaml", "json"])
+@pytest.mark.parametrize("obj", [AlwaysBaz(foo="always"), CopyBaz(foo="copy")])
+def test_nested_subclass(
+    obj: AbstractBaz, format: Literal["auto", "zip", "folder", "yaml", "json"], tmpdir: str
+):
+    """Test round-trip of objects with a nested subclass."""
+    # Initial round-trip (should always work)
+    save_model(obj, f"{tmpdir}/obj", format=format)
+    obj2 = load_model(f"{tmpdir}/obj", AbstractBaz)
+    assert obj.foo == obj2.foo
+    assert obj.get_baz() == obj2.get_baz()
+    # Nested round-trip (should also always work, but is more diffictult)
+    nest = Bazzifier(maker=obj)
+    save_model(nest, f"{tmpdir}/nest", format=format)
+    # nest2 = load_model(f"{tmpdir}/nest", Bazzifier)
+    # assert nest.maker == nest2.maker
+    # assert nest.maker.get_baz() == nest2.maker.get_baz()
