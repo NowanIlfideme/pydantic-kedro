b3bc1d0e827f0ba67eec70d2fb78859ead3a6712
NowanIlfideme <git@nowan.dev>
2023-06-16 13:16:18 +0200

    Renamed, added another test.

--- src/test/test_strict.py
+++ src/test/test_strict.py
@@ -1,4 +1,4 @@
-"""Test issue with settings."""
+"""Test strict models and BaseSettings subclasses."""
 
 import pytest
 from pydantic import BaseModel, BaseSettings
@@ -20,6 +20,17 @@ class ExModel(BaseModel):
     settings: ExSettings
 
 
+class StrictModel(BaseModel):
+    """Strict no-extras values."""
+
+    val: str
+
+    class Config:
+        """Pydantic model configuration."""
+
+        extra = "forbid"
+
+
 @pytest.mark.parametrize("format", ["auto", "zip", "folder", "yaml", "json"])
 def test_rt_settings(tmpdir: str, format: Literal["auto", "zip", "folder", "yaml", "json"]):
     """Test settings round-trip."""
@@ -27,3 +38,12 @@ def test_rt_settings(tmpdir: str, format: Literal["auto", "zip", "folder", "yaml
     save_model(obj, f"{tmpdir}/obj", format=format)
     obj2 = load_model(f"{tmpdir}/obj", ExModel)
     assert obj.settings == obj2.settings
+
+
+@pytest.mark.parametrize("format", ["auto", "zip", "folder", "yaml", "json"])
+def test_rt_strict_model(tmpdir: str, format: Literal["auto", "zip", "folder", "yaml", "json"]):
+    """Test strict_model round-trip."""
+    obj = StrictModel(val="val")
+    save_model(obj, f"{tmpdir}/obj", format=format)
+    obj2 = load_model(f"{tmpdir}/obj", StrictModel)
+    assert obj.val == obj2.val
